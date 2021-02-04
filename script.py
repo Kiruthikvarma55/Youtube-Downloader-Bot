9a9014d5236e93b588a7f7138b90fe5b0d0278a6
Kiruthikvarma55 <75213972+Kiruthikvarma55@users.noreply.github.com>
2021-02-04 12:22:05 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,30 @@
+class script(object):
+
+    START_MSG = """ <b>Hey {},
+
+     I'm A Simple but POWERFUL You Tube URL Uploader Bot...💯
+
+<b>Please send me a YouTube video Link, I can upload to telegram as File/Video!!!</b>
+
+/help more details...."""
+    HELP_MSG = """Hello, It's not Difficult!!!
+
+<b>Follow these Steps...</b>
+
+<code>⛳️ Send your YouTube Video link.</code>
+
+<code>⛳️ Select desired option.</code>
+
+<code>⛳️ That's all, It's simple.</code>
+
+📝 Currently, Only supports Youtube Single Video (No playlist).
+
+
+<b>© @Kalam_Company</b>"""
+    ABOUT_MSG = """⭕️ <b>My Name : Kalam YouTube DownloadeR</b>
+
+📖 <b>Language :</b> <code>Python3</code>
+
+🔥 <b>Creater :</b>© @Kalam_Company
+
+🥳 <b>My Birthday:-</b> 05.02.2021"""
